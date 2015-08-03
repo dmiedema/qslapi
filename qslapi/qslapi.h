398da07ccd26c46904ef665f1bc440cf447178bb
dmiedema <danielmiedema@me.com>
2015-08-02 20:37:47 -0700

    Initial Commit

---
+++ qslapi/qslapi.h
@@ -0,0 +1,19 @@
+//
+//  qslapi.h
+//  qslapi
+//
+//  Created by Daniel Miedema on 8/2/15.
+//  Copyright © 2015 Quack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for qslapi.
+FOUNDATION_EXPORT double qslapiVersionNumber;
+
+//! Project version string for qslapi.
+FOUNDATION_EXPORT const unsigned char qslapiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <qslapi/PublicHeader.h>
+
+
